120efd58274cb29ca567fef9ba4fd1ee13a37615
Transparency010101 <ossys1010110@gmail.com>
2023-01-22 22:09:02 +0200

    Оно бесконечно скачивает файлы

--- core.py
+++ core.py
@@ -1,58 +1,44 @@
 import requests
-import random
 
+from random import choice
+from time import sleep
 from bs4 import BeautifulSoup
 
+from other import *
 
-def create_random_six_symbols():
-    lower_case = "abcdefghijklmnopqrstuvwxyz"
-    numbers = "1234567890"
-    use_for = lower_case + numbers
-    length_for_pass = 6
-    result = "".join(random.sample(use_for, length_for_pass))
-
-    return result
+number_of_images = input("How many images to download(default 10): ") or 10
 
 
-desktop_agents = [
-    # Tor Browser for Windows and Linux
-    'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',
-    # Tor Browser for Android
-    'Mozilla/5.0 (Android 10; Mobile; rv:91.0) Gecko/91.0 Firefox/91.0',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) '
-    'AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) '
-    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) '
-    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
-]
+def main():
+    header = {
+        "User-Agent": None,
+        "Accept": "*/*"
+    }
 
-header = {
-    "User-Agent": None,
-    "Accept": "*/*"
-}
+    seconds = 2
+    sleep(seconds)
 
-number_of_images = input("How many images to download(default 3): ") or 3
-
-for i in range(int(number_of_images)):
-    header["User-Agent"] = random.choice(desktop_agents)
+    header["User-Agent"] = choice(desktop_agents)
 
     site_with_img = requests.get(
         f"https://prnt.sc/image/{create_random_six_symbols()}",
         headers=header
     ).text
 
-    img_url = BeautifulSoup(site_with_img, "lxml").find("img", {"id": "screenshot-image"}).get("src")
+    img_url = BeautifulSoup(site_with_img, "lxml").find("img", {
+        "id": "screenshot-image"
+    }).get("src")
 
     img_request = requests.get(img_url, headers=header)
-    img_opt = open(f"img/test_{i}" + ".jpg", "wb")
+    img_opt = open(
+        f"img/test_{create_random_name_for_img()}" + ".jpg", "wb"
+    )
     img_opt.write(img_request.content)
 
+
+if __name__ == "__main__":
+    while True:
+        try:
+            main()
+        except Exception:
+            continue
