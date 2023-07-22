8c678277feb958ce0866ae06716794779a121df6
Ossys <ossys@gmail.com>
2023-07-22 04:10:01 +0300

    foget to change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrab_img_from_ligthshot',
-    version='2.2.0',
+    version='2.3.0',
     description="scrab images from ligthshot (i don't know is this legal)",
     url="https://github.com/Transparency010101/scrab-img-from-ligthshot",
     install_requires=[
